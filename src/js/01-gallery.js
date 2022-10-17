b3b29e18e29764d8a1211ed4a8ea94113c25a12a
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-10-17 08:10:13 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+// Change code below this line
+let gallery = document.querySelector(".gallery");
+
+
+gallery.insertAdjacentHTML('afterbegin',createItem(galleryItems));//---  << Вставка разметки >>
+
+function createItem(array) {return array.map(({preview,original,description}) => {//---  << Созд. разметки >>
+  return`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join("")};
+
+const modalWindow = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});//---  << Созд. БИБЛИОТЕКОЙ мод. окна >>
+
+modalWindow.on('show.simplelightbox', function () {});
