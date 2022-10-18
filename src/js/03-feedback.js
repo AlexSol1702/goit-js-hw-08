d11ba164962639f1b48d31ca13cb187a74022fe6
AlexSol1702 <105703672+AlexSol1702@users.noreply.github.com>
2022-10-18 21:39:09 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import  throttle  from "lodash.throttle";
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea')
+};
+
+const STORAGE_KEY = "feedback-form-state";
+let data = {"email": "","message": ""};
+
+refs.form.addEventListener('input', throttle(addStorage, 500))
+
+function addStorage(e) {
+    if (e.target === refs.email) {
+        data.email = e.target.value;
+    } if(e.target === refs.message) {
+        data.message = e.target.value;
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+}
+
+if (localStorage.getItem(STORAGE_KEY)) {
+    data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+}
+    refs.email.value = data.email;
+    refs.message.value = data.message;
+
+refs.form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    if (refs.email.value !== '' && refs.message.value !== '') {
+    console.log(data);
+    localStorage.removeItem(STORAGE_KEY);
+    e.target.reset();
+    } 
+})
+
 
